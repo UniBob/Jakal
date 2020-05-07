ef23816db3cf0710ac4be64c9ddf03341fb78cbe
UniBob <33136825+UniBob@users.noreply.github.com>
2020-05-07 19:03:19 +0300

    Add player clip

---
+++ Assets/PlayerClipScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerClipScript : MonoBehaviour
+{
+    public int clipTag;
+    bool isActive;
+    GameManager gm;
+
+    private void Start()
+    {
+        isActive = false;
+        gm = FindObjectOfType<GameManager>();
+    }
+
+    public void SetActive(bool set)
+    {
+        isActive = set;
+    }
+
+    public bool GetActive()
+    {
+        return isActive;
+    }
+
+    private void OnMouseDown()
+    {
+        if (isActive)
+        {
+            gm.StartPanelSelect(clipTag);
+            isActive = false;
+        }
+    }
+}
