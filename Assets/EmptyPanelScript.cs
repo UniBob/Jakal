17ec4f1cabee3d9679f9f396b107528bd32f3d7f
UniBob <33136825+UniBob@users.noreply.github.com>
2020-05-09 15:25:12 +0300

    Add Empty Panel Script

---
+++ Assets/EmptyPanelScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmptyPanelScript : MonoBehaviour
+{
+    public int panelTag;
+
+    bool isActive;
+    GameManager gm;
+
+    private void Start()
+    {
+        isActive = false;
+        gm = GetComponent<GameManager>();
+    }
+
+    private void OnMouseDown()
+    {
+        if (isActive)
+        {
+            gm.StartClipSelect(panelTag);
+        }
+    }
+
+    public void SetActive(bool tmp)
+    {
+        isActive = tmp;
+    }
+
+    public bool GetActive()
+    {
+        return isActive;
+    }
+}
