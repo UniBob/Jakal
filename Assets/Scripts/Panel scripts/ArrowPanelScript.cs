86847e03ec0026279372aa31a42ca9a63349d0c1
UniBob <33136825+UniBob@users.noreply.github.com>
2020-06-24 14:30:12 +0300

    Revert "Revert "1""
    This reverts commit f6768027f9b0dcaa688cedff6e6af6503c4a45bb.

--- Assets/Scripts/Panel scripts/ArrowPanelScript.cs
+++ Assets/Scripts/Panel scripts/ArrowPanelScript.cs
@@ -4,19 +4,57 @@ using UnityEngine;
 
 public class ArrowPanelScript : MonoBehaviour
 {
-    bool[] directions = new bool[9];
+    bool[] directions = new bool[8];
 
     public void panelOption(int panelTag, GameManager gm)
     {
         gm.MoveClipWithoutEndingTurn(panelTag);
-        for(int i = 0;i<9;i++)
+        for(int i = 0;i<8;i++)
         {
             if (directions[i])
             {
+                Vector3 tmp = Vector3.zero;
                 switch(i)
                 {
                     case 0:
-                        ActivatePanel
+                        tmp.x = -1;
+                        gm.ActivatePanel(tmp);
+                        break;
+                    case 1:
+                        tmp.x = 1;
+                        gm.ActivatePanel(tmp);
+                        break;
+                    case 2:
+                        tmp.x = -1;
+                        tmp.y = -1;
+                        gm.ActivatePanel(tmp);
+                        break;
+                    case 3:
+                        tmp.x = -1;
+                        tmp.y = 1;
+                        gm.ActivatePanel(tmp);
+                        break;
+                    case 4:
+                        tmp.x = 1;
+                        tmp.y = -1;
+                        gm.ActivatePanel(tmp);
+                        break;
+                    case 5:
+                        tmp.x = 1;
+                        tmp.y = 1;
+                        gm.ActivatePanel(tmp);
+                        break;
+                    case 6:
+                        tmp.y = -1;
+                        gm.ActivatePanel(tmp);
+                        break;
+                    case 7:
+                        tmp.y = -1;
+                        gm.ActivatePanel(tmp);
+                        break;
+                    case 8:
+                        tmp.y = 1;
+                        gm.ActivatePanel(tmp);
                         break;
                 }
             }
