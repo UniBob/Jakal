00a6c39549d8e47c7a6f4e00d7ff63530522f684
UniBob <33136825+UniBob@users.noreply.github.com>
2020-06-24 14:30:37 +0300

    Revert "1"
    This reverts commit e3255e6f2656d81e223a62408fea525bf3d55c0f.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public PlayerClipScript[] clips;
+    public int playerID;
+
+    GameManager gm;
+    private void Start()
+    {
+        gm = FindObjectOfType<GameManager>();
+        int j = 0;
+        foreach (PlayerClipScript i in clips)
+        {
+            i.SetTag(j);
+            j++;
+        }
+    }
+
+    public void StartTurn()
+    {
+        foreach (PlayerClipScript i in clips)
+        {
+            i.SetActive(true);
+        }
+    }
+
+    public void StartPanelSelect(int playerClipTag)
+    {
+        foreach(PlayerClipScript i in clips)
+        {
+            i.SetActive(false);
+        }
+        gm.StartPanelSelect(playerClipTag);
+    }
+
+    public void MoveClip(Vector3 newCoord, int playerClipTag)
+    {
+        clips[playerClipTag].Move(newCoord);
+    }
+
+    public void SetID(int id)
+    {
+        playerID = id;
+    }
+    
+    public Vector3 GetClipCoord(int clipTag)
+    {
+        return clips[clipTag].GetCoord();
+    }
+
+
+}
